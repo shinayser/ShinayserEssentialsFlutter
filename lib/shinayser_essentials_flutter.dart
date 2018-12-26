0ed5c2a75a6f559be47f132d8b3167deda9cd8df
Daniel Oliveira <danieloliveira@denox.com.br>
2018-12-26 11:53:33 -0200

    Changing main file to export other files

--- lib/shinayser_essentials_flutter.dart
+++ lib/shinayser_essentials_flutter.dart
@@ -1,7 +1,10 @@
-library shinayser_essentials_flutter;
+//library shinayser_essentials_flutter;
+//
+///// A Calculator.
+//class Calculator {
+//  /// Returns [value] plus 1.
+//  int addOne(int value) => value + 1;
+//}
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'utils/utils.dart';
+export 'animation/showup.dart';
