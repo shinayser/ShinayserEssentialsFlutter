7b208ca5bdcda5b4e950950d3e45d3b75bfa5ada
Daniel Oliveira <danieloliveira@denox.com.br>
2018-12-26 11:57:02 -0200

    Version 1.0.0

--- lib/shinayser_essentials_flutter.dart
+++ lib/shinayser_essentials_flutter.dart
@@ -1,10 +1,4 @@
-//library shinayser_essentials_flutter;
-//
-///// A Calculator.
-//class Calculator {
-//  /// Returns [value] plus 1.
-//  int addOne(int value) => value + 1;
-//}
+library shinayser_essentials_flutter;
 
 export 'utils/utils.dart';
 export 'animation/showup.dart';
