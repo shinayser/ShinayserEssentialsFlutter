78983d18e3c60e2d7fff9d71ceab8de8bf15d5dc
Daniel Oliveira <shinayser@gmail.com>
2019-03-12 20:22:16 -0300

    - Added AnimationControllerOwnerMixin - More auxiliary constructors for ShowUp and GrowUp

--- lib/shinayser_essentials_flutter.dart
+++ lib/shinayser_essentials_flutter.dart
@@ -3,3 +3,5 @@ library shinayser_essentials_flutter;
 export 'utils/utils.dart';
 export 'utils/fade_page_route.dart';
 export 'animation/showup.dart';
+export 'animation/growup.dart';
+export 'animation/animation_controller_owner_mixin.dart';
