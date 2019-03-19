dc498999d51b3caea342b1d8e7a9dacc955adbeb
Daniel Oliveira <shinayser@gmail.com>
2019-03-19 19:30:23 -0300

    Added DialogDrawer

--- lib/shinayser_essentials_flutter.dart
+++ lib/shinayser_essentials_flutter.dart
@@ -6,3 +6,4 @@ export 'animation/showup.dart';
 export 'animation/growup.dart';
 export 'animation/animated_scale.dart';
 export 'animation/animation_controller_owner_mixin.dart';
+export 'widgets/dialog_drawer.dart';
